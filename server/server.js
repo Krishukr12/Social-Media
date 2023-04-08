c8e17df67fd124d92a4410d0b0db2b7d19810e18
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2023-04-08 17:26:08 +0530

    DB : mongodb configuration completed

--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
 const express = require("express");
+const cors = require("cors");
+
 const connection = require("./config/db.js");
-
 require("dotenv").config();
-
-const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+//!.... Middleware
+app.use(express.json());
+app.use(cors());
+
+//!.... Routes
 app.get("/", (req, res) => {
   res.send("HomePage");
 });
 
-app.listen(PORT, async () => {
+//!.... Start the server
+app.listen(process.env.PORT || 8080, async () => {
   try {
     await connection;
     console.log("Connected to mongoDB");
-    console.log(`server is running on ${PORT}`);
+    console.log(`server is running on ${process.env.PORT}`);
   } catch (error) {
     console.log(error);
   }
