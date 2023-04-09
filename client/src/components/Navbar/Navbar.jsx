91b86c2251970ae3f4d586a8ff1253c7e382559f
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2023-04-10 01:22:26 +0530

    Seeting up css for active option

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom";
 import classes from "./Navbar.module.css";
+import { useState } from "react";
 export const Navbar = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
     <div className={classes.navContainer}>
       <div className={classes.logo_container}>
-        <Link to="/">Social Media</Link>
+        <Link onClick={() => setActiveNav("#")} to="/">
+          Social Media
+        </Link>
       </div>
       <div className={classes.link_container}>
-        <Link className={classes.link} to="/">
+        <Link
+          onClick={() => setActiveNav("/")}
+          className={activeNav === "/" ? classes.link : null}
+          to="/"
+        >
           Home
         </Link>
-        <Link className={classes.link} to="/analytics">
+        <Link
+          onClick={() => setActiveNav("/analytics")}
+          className={activeNav === "/analytics" ? classes.link : null}
+          to="/analytics"
+        >
           Analytics
         </Link>
       </div>
